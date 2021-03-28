eb62caead5fd8fccb8ef59c787828d045af44c46
mersede <mersede.rahbar@gmail.com>
2021-03-28 19:46:24 +0430

    add shot

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import shot from 'assets/images/shot.png';
 import Header from 'Components/Header';
 
 function App() {
@@ -7,6 +8,18 @@ function App() {
     <>
       <div className="layout">
         <Header />
+        <div className="content">
+          <div className="container-fluid">
+            <div className="row justify-content-center">
+              <div className="col-8 text-center">
+                <div className="header-info">Conflux wallet</div>
+                <h1 className="header-title">Powerful and simple Conflux desktop wallet</h1>
+                <p className="header-desc">Flox is an open-source desktop wallet that allows you to manage your assets on the Conflux network.</p>
+              </div>
+            </div>
+            <img src={shot} className="header-shot" alt="fullet"/>
+          </div>
+        </div>
       </div>
     </>
   );
